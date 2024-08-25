bbce16405f8c86eaaa84e0de71c8863ba70f2b51
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2024-08-25 13:09:14 -0300

    checkbox adicionada para filtrar quadros

--- script.js
+++ script.js
@@ -1,44 +1,92 @@
 let meuBotao = document.querySelector('button');
-
 meuBotao.addEventListener('click', showImage);
 
 function showImage(){
-    
     let txtSearch = document.querySelector('#theLostCanva');
     let pesquisa = txtSearch.value;
-    
     const urlDataCanva = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=" + pesquisa;
 
-    let container = document.getElementById('container')
+    try {
+        let cont = document.getElementById('container')
+        cont.remove()
+    } catch (error) {
+        console.error(error);
+    }
 
+    let ywch = document.getElementById('god')
+
+    let container = document.createElement('div')
+    container.id = 'container'
+    container.className = 'row'
+    
+    let checkCanva = document.getElementById('onlyCanva')
+
+    
     fetch(urlDataCanva)
-        .then(response => response.json())
-        .then(data => {
-            for (let index = 0; index < data.total; index++) {
-                
-                let dados = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + data.objectIDs[index]
-                fetch(dados)
-                .then(response => response.json())
-                .then(data2 => {
-                    if (data2.primaryImageSmall == "" || data2.title == "" || data2.title == "undefined" || data2.message == "Not a valid object" || !data2.primaryImage) {
+    .then(response => response.json())
+    .then(data => {
+        for (let index = 0; index < data.total; index++) {
+            let dados = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + data.objectIDs[index]
+            fetch(dados)
+            .then(response => response.json())
+            .then(data2 => {
+                try {
+                    if (!data2.primaryImage || data2.primaryImage == "") {
                         index++
                     } else {
-                        let newDiv = document.createElement('div')
-                        newDiv.className = 'arts col-3'
-                        let newImg = document.createElement('img')
-                        newImg.src = data2.primaryImageSmall
-                        let title = document.createElement('div')
-                        title.id = 'title'+index
-                        let author = document.createElement('div')
-                        author.id = 'author'+index
-                        container.appendChild(newDiv)
-                        newDiv.appendChild(newImg)
-                        newDiv.appendChild(title)
-                        newDiv.appendChild(author)
-
-                        document.getElementById('title'+index).innerText = "title: " + data2.title 
-                        document.getElementById('author'+index).innerText = 'author: '+ data2.constituents[0].name
+                        if (checkCanva.checked == true) {
+                            if (data2.objectName == "Painting" || data2.objectName == "Drawing") {
+                                let newDiv = document.createElement('div')
+                                newDiv.className = 'arts col-3'
+                                let title = document.createElement('div')
+                                title.id = 'title'+index
+                                let author = document.createElement('div')
+                                author.id = 'author'+index
+                                author.onclick="creator(data2.constituents[0].name)"
+                                let fullImg = document.createElement('a')
+                                fullImg.href = data2.primaryImage
+                                fullImg.target = 'blank'
+                                let newImg = document.createElement('img')
+                                newImg.src = data2.primaryImageSmall
+                                ywch.appendChild(container)
+                                container.appendChild(newDiv)
+                                newDiv.appendChild(fullImg)
+                                newDiv.appendChild(title)
+                                newDiv.appendChild(author)
+                                fullImg.appendChild(newImg)
+        
+                                document.getElementById('title'+index).innerText = "title: " + data2.title 
+                                document.getElementById('author'+index).innerText = 'author: '+ data2.constituents[0].name
+                            } else {
+                                index++
+                            }
+                        } else {
+                            let newDiv = document.createElement('div')
+                            newDiv.className = 'arts col-3'
+                            let title = document.createElement('div')
+                            title.id = 'title'+index
+                            let author = document.createElement('div')
+                            author.id = 'author'+index
+                            author.onclick="creator(data2.constituents[0].name)"
+                            let fullImg = document.createElement('a')
+                            fullImg.href = data2.primaryImage
+                            fullImg.target = 'blank'
+                            let newImg = document.createElement('img')
+                            newImg.src = data2.primaryImageSmall
+                            ywch.appendChild(container)
+                            container.appendChild(newDiv)
+                            newDiv.appendChild(fullImg)
+                            newDiv.appendChild(title)
+                            newDiv.appendChild(author)
+                            fullImg.appendChild(newImg)
+    
+                            document.getElementById('title'+index).innerText = "title: " + data2.title 
+                            document.getElementById('author'+index).innerText = 'author: '+ data2.constituents[0].name
+                        }
                     }
+                } catch (error) {
+                    
+                }
                 })
             }
         })
@@ -46,3 +94,4 @@ function showImage(){
             console.error(`deu merda: ${error}`);
         })
 }
+
