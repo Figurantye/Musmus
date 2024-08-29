ab0155187c954adc9901829470c56665bb6871c4
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2024-08-28 23:44:35 -0300

    classificando mais pinturas como 'canva'

--- script.js
+++ script.js
@@ -21,77 +21,84 @@ function showImage(){
     
     let checkCanva = document.getElementById('onlyCanva')
 
-    
     fetch(urlDataCanva)
-    .then(response => response.json())
-    .then(data => {
-        for (let index = 0; index < data.total; index++) {
-            let dados = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + data.objectIDs[index]
-            fetch(dados)
-            .then(response => response.json())
-            .then(data2 => {
+        .then(response => response.json())
+        .then(data => {
+            for (let index = 0; index < data.total; index++) {
+                let dados = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + data.objectIDs[index]
+                let total = document.getElementById('total')
                 try {
-                    if (!data2.primaryImage || data2.primaryImage == "") {
-                        index++
-                    } else {
-                        if (checkCanva.checked == true) {
-                            if (data2.objectName == "Painting" || data2.objectName == "Drawing") {
-                                let newDiv = document.createElement('div')
-                                newDiv.className = 'arts col-3'
-                                let title = document.createElement('div')
-                                title.id = 'title'+index
-                                let author = document.createElement('div')
-                                author.id = 'author'+index
-                                author.onclick="creator(data2.constituents[0].name)"
-                                let fullImg = document.createElement('a')
-                                fullImg.href = data2.primaryImage
-                                fullImg.target = 'blank'
-                                let newImg = document.createElement('img')
-                                newImg.src = data2.primaryImageSmall
-                                ywch.appendChild(container)
-                                container.appendChild(newDiv)
-                                newDiv.appendChild(fullImg)
-                                newDiv.appendChild(title)
-                                newDiv.appendChild(author)
-                                fullImg.appendChild(newImg)
-        
-                                document.getElementById('title'+index).innerText = "title: " + data2.title 
-                                document.getElementById('author'+index).innerText = 'author: '+ data2.constituents[0].name
-                            } else {
-                                index++
-                            }
-                        } else {
-                            let newDiv = document.createElement('div')
-                            newDiv.className = 'arts col-3'
-                            let title = document.createElement('div')
-                            title.id = 'title'+index
-                            let author = document.createElement('div')
-                            author.id = 'author'+index
-                            author.onclick="creator(data2.constituents[0].name)"
-                            let fullImg = document.createElement('a')
-                            fullImg.href = data2.primaryImage
-                            fullImg.target = 'blank'
-                            let newImg = document.createElement('img')
-                            newImg.src = data2.primaryImageSmall
-                            ywch.appendChild(container)
-                            container.appendChild(newDiv)
-                            newDiv.appendChild(fullImg)
-                            newDiv.appendChild(title)
-                            newDiv.appendChild(author)
-                            fullImg.appendChild(newImg)
-    
-                            document.getElementById('title'+index).innerText = "title: " + data2.title 
-                            document.getElementById('author'+index).innerText = 'author: '+ data2.constituents[0].name
-                        }
-                    }
+                    total.innerText = 'results: ' + data.total
                 } catch (error) {
-                    
+                    total.innerText = 'results: 0'
+                    console.error(error);
+
                 }
-                })
-            }
-        })
-        .catch(error => {
-            console.error(`deu merda: ${error}`);
+                fetch(dados)
+                    .then(response => response.json())
+                    .then(data2 => {
+                        try {
+                            if (!data2.primaryImage || data2.primaryImage == "") {
+                                index++
+                            } else {
+                                if (checkCanva.checked == true) {
+                                    if (data2.objectName == "Painting" || data2.objectName == "Drawing" || data2.objectName == "Folio" || data2.objectName == "Tangka" || data2.objectName == "Wall painting" || data2.objectName == "Hanging scroll" || data2.objectName == "Print" || data2.objectName == "Handscroll" || data2.objectName == "Woodblock print" || data2.objectName == "Screens") {
+                                        let newDiv = document.createElement('div')
+                                        newDiv.className = 'arts col-3'
+                                        let title = document.createElement('div')
+                                        title.id = 'title'+index
+                                        let author = document.createElement('div')
+                                        author.id = 'author'+index
+                                        let fullImg = document.createElement('a')
+                                        fullImg.href = data2.primaryImage
+                                        fullImg.target = 'blank'
+                                        let newImg = document.createElement('img')
+                                        newImg.src = data2.primaryImageSmall
+
+                                        ywch.appendChild(container)
+                                        container.appendChild(newDiv)
+                                        newDiv.appendChild(fullImg)
+                                        newDiv.appendChild(title)
+                                        newDiv.appendChild(author)
+                                        fullImg.appendChild(newImg)
+                                    
+                                        document.getElementById('title'+index).innerText = "title: " + data2.title 
+                                        document.getElementById('author'+index).innerText = 'author: '+ data2.constituents[0].name
+                                    } else {
+                                        index++
+                                    }
+                                } else {
+                                    let newDiv = document.createElement('div')
+                                    newDiv.className = 'arts col-3'
+                                    let title = document.createElement('div')
+                                    title.id = 'title'+index
+                                    let author = document.createElement('div')
+                                    author.id = 'author'+index
+                                    author.onclick="creator(data2.constituents[0].name)"
+                                    let fullImg = document.createElement('a')
+                                    fullImg.href = data2.primaryImage
+                                    fullImg.target = 'blank'
+                                    let newImg = document.createElement('img')
+                                    newImg.src = data2.primaryImageSmall
+                                    ywch.appendChild(container)
+                                    container.appendChild(newDiv)
+                                    newDiv.appendChild(fullImg)
+                                    newDiv.appendChild(title)
+                                    newDiv.appendChild(author)
+                                    fullImg.appendChild(newImg)
+                                
+                                    document.getElementById('title'+index).innerText = "title: " + data2.title 
+                                    document.getElementById('author'+index).innerText = 'author: '+ data2.constituents[0].name
+                                }
+                            }
+                        } catch (error) {
+
+                        }
+                        })
+                    }
+                    })  
+                .catch(error => {
+                    console.error(`deu merda: ${error}`);
         })
 }
 
